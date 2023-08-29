a42f7b52a42c26309637c4cfdf1df3b80ad12dbb
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-08-29 15:42:34 -0600

    INIT

---
+++ 2- PY/3- Nivel Avanazado/3.1 Tu pass WIFI/script.py
@@ -0,0 +1,7 @@
+"""Titulo: |Tu pass WIFI"""
+
+#####################################################################################
+# Descripción: <Script,te muestra las contraseñas de tu wifi ocultas en tu maquina>
+# Autor: <DuskStar>
+# Fecha de creación: <29/08/2023>
+#####################################################################################
