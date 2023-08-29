ab28908217d8705f023e7d05669cea9627b24681
Speencer Pulido Romero <134586747+DuskStarGITHUB@users.noreply.github.com>
2023-08-29 16:14:21 -0600

    CODIGO PULIDO

--- 2- PY/3- Nivel Avanazado/3.1 Tu pass WIFI/script.py
+++ 2- PY/3- Nivel Avanazado/3.1 Tu pass WIFI/script.py
@@ -9,13 +9,18 @@
 import subprocess
 
 def get_wifi_passwords():
+    """Procesos"""
     # Obtener los nombres de las redes
-    output_profiles = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output=True, text=True, shell=True)
-    profile_names = [line.split(":")[1].strip() for line in output_profiles.stdout.splitlines() if "Perfil de todos los usuarios" in line]
+    output_profiles = subprocess.run(
+        ["netsh", "wlan", "show", "profiles"],
+        capture_output=True, text=True, shell=True, check=False)
+    profile_names = [
+        line.split(":")[1].strip()
+        for line in output_profiles.stdout.splitlines() if "Perfil de todos los usuarios" in line]
     wifi_passwords = []
     for profile_name in profile_names:
         cmd = f"netsh wlan show profile name=\"{profile_name}\" key=clear"
-        output = subprocess.run(cmd, capture_output=True, text=True, shell=True)
+        output = subprocess.run(cmd, capture_output=True, text=True, shell=True, check=False)
         lines = output.stdout.splitlines()
         for line in lines:
             if "Contenido de la clave" in line:
@@ -24,6 +29,7 @@ def get_wifi_passwords():
     return wifi_passwords
 
 def main():
+    """Funcion Principal"""
     wifi_passwords = get_wifi_passwords()
     if len(wifi_passwords) == 0:
         print("No se encontraron contraseñas de redes Wi-Fi.")
