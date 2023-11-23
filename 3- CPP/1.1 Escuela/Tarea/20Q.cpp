56a6da9bb0bc42db4fff02442fd30e81ffa2ad96
DuskStar <contactspeencer@gmail.com>
2023-11-23 14:15:34 -0600

    TASK menu finished

--- 3- CPP/1.1 Escuela/Tarea/20Q.cpp
+++ 3- CPP/1.1 Escuela/Tarea/20Q.cpp
@@ -12,11 +12,42 @@ using namespace std;
 // Funcion principal
 int main()
 {
+    // Variables
+    string menu;
     // Despliegue de menu
-    cout << "\t\t\tBienvenido a 20Q" << endl;
-    cout << "\nElija una opcion mentalmente y intentare adivinarlo:\n"<< endl;
-    cout << "\n\tFrutas:\n\tTecojote\tMango\tCoco" << endl;
-    cout << "\n\tAnimales:\n\tPajaro\tCanguro\tLeon" << endl;
-    cout << "\n\tObjetos:\n\tCanica\tEscoba\tCubeta\n\n" << endl;
+    do
+    {
+        cout << "\t\t\t\tBienvenido a 20Q" << endl;
+        cout << "\nElija una opcion mentalmente d ela siguiente lista y intentare adivinar a traves de una serie de maximo 20 preguntas que opcion has elegido.\n"
+             << endl;
+        cout << "\n\tFrutas:\n\tTecojote\tMango\t\tCoco" << endl;
+        cout << "\n\tAnimales:\n\tPajaro\t\tCanguro\t\tLeon" << endl;
+        cout << "\n\tObjetos:\n\tCanica\t\tEscoba\t\tCubeta\n\n"
+             << endl;
+        // Pregunta para repetir o finalizar juego juego
+        cout << "¿Quieres jugar de nuevo? (S/N): ";
+        cin >> menu;
+        // Validacion de respuesta del usuario
+        if (menu == "S" or menu == "s")
+        {
+            cout << "¡Okay hagamoslo de nuevo!" << endl;
+        }
+        else if (menu == "N" or menu == "n")
+        {
+            continue;
+        }
+        else
+        {
+            do
+            {
+                cout << "Opcion no valida" << endl;
+                cout << "¿Quieres jugar de nuevo? (S/N): ";
+                cin >> menu;
+            } while (menu != "S" and menu != "s" and menu != "N" and menu != "n");
+        }
+    } while (menu != "N" and menu != "n");
+    // Mensaje de finalizacion del juego
+    cout << "Gracias por jugar" << endl;
+    // Fin del Programa
     return 0;
 }
