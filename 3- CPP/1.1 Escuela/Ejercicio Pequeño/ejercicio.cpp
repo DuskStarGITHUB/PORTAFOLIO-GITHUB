9ab8e7dab157c13ee4717f7b446fb45f47054448
DuskStar <contactspeencer@gmail.com>
2023-12-08 13:11:41 -0600

    main

---
+++ 3- CPP/1.1 Escuela/Ejercicio Pequeño/ejercicio.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b, res;
+    cout << "Dame el valor de A: ";
+    cin >> a;
+    cout << "Dame el valor de B: ";
+    cin >> b;
+    res = a + b;
+    cout << "El resultado de la suma es: " << res << endl;
+    return 0;
+}
